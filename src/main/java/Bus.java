6f824d363057a48fe3c54df2e54b305e402715f9
yanazykova <yvz1973@gmail.com>
2020-11-17 15:46:24 +0000

    Added Person Class and Add person test passing

--- src/main/java/Bus.java
+++ src/main/java/Bus.java
@@ -1,19 +1,31 @@
-import javax.print.attribute.standard.Destination;
 import java.util.ArrayList;
 
 public class Bus {
 
     private String destination;
     private int capacity;
-//    private ArrayList<Person> passengers;
+    private ArrayList<Person> passengers;
 
     public Bus(String destination, int capacity){
         this.destination = destination;
         this.capacity = capacity;
+        this.passengers = new ArrayList<Person>();
     }
 
     public String getDestination(){
         return this.destination;
+    }
+
+    public int getCapacity(){
+        return this.capacity;
+    }
+
+    public int passengerCount(){
+        return this.passengers.size();
+    }
+
+    public void addPerson(Person person){
+        this.passengers.add(person);
     }
 }
 
--- src/test/java/BusTest.java
+++ src/test/java/BusTest.java
@@ -6,15 +6,43 @@ import static org.junit.Assert.assertEquals;
 public class BusTest {
 
     private Bus bus;
+    private Person person;
 
     @Before
     public void before() {
-        bus = new Bus("Balerno", 80);
+        bus = new Bus("Balerno", 5);
+        person = new Person();
     }
 
     @Test
     public void hasDestination() {
         assertEquals("Balerno", bus.getDestination());
+    }
+
+    @Test
+    public void hasCapacity() {
+        assertEquals(5, bus.getCapacity());
+    }
+
+    @Test
+    public void startsEmpty() {
+        assertEquals(0, bus.passengerCount());
+    }
+
+    @Test
+    public void canAddPerson() {
+        this.bus.addPerson(person);
+        assertEquals(1, bus.passengerCount());
+    }
+
+    @Test
+    public void canOnlyAddPassengersIfBelowCapacity() {
+        this.bus.addPerson(person);
+        this.bus.addPerson(person);
+        this.bus.addPerson(person);
+        this.bus.addPerson(person);
+        this.bus.addPerson(person);
+        assertEquals();
     }
 
 
